844bc4a8cdbd31f591b280fdf88a9d5ef02d2d9d
[dyad] <git@dyad.sh>
2026-06-03 20:30:46 +0200

    [dyad] Removed Catalog and RentalCalculator from Prenajom page, keeping only EquipmentCatalog - wrote 1 file(s)

--- src/pages/Prenajom.tsx
+++ src/pages/Prenajom.tsx
@@ -2,9 +2,7 @@
 
 import React from 'react';
 import Navbar from '@/components/Navbar';
-import Catalog from '@/components/Catalog';
 import EquipmentCatalog from '@/components/EquipmentCatalog';
-import RentalCalculator from '@/components/RentalCalculator';
 import Footer from '@/components/Footer';
 
 const Prenajom = () => {
@@ -12,9 +10,7 @@ const Prenajom = () => {
     <main className="min-h-screen bg-[#020721]">
       <Navbar />
       <div className="pt-20">
-        <Catalog />
         <EquipmentCatalog />
-        <RentalCalculator />
       </div>
       <Footer />
     </main>
