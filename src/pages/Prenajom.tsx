e5e24f3104df1ee912e6336c86c0ebc6251bf237
[dyad] <git@dyad.sh>
2026-06-03 20:34:43 +0200

    Reverted all changes back to version e3b1321ba016e2bccbe25d7c9359427637b0b403

--- src/pages/Prenajom.tsx
+++ src/pages/Prenajom.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import Catalog from '@/components/Catalog';
-import EquipmentCatalog from '@/components/EquipmentCatalog';
 import RentalCalculator from '@/components/RentalCalculator';
 import Footer from '@/components/Footer';
 
@@ -13,7 +12,6 @@ const Prenajom = () => {
       <Navbar />
       <div className="pt-20">
         <Catalog />
-        <EquipmentCatalog />
         <RentalCalculator />
       </div>
       <Footer />
