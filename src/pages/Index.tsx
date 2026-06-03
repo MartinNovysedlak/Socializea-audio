ae71edadbea575bfb3fc0a10d339f922aff56b9a
[dyad] <git@dyad.sh>
2026-06-03 19:58:23 +0200

    [dyad] Removing the packages catalog from the home page - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import RentalSummary from '@/components/RentalSummary';
-import Catalog from '@/components/Catalog';
 import DJSection from '@/components/DJSection';
 import SalesSummary from '@/components/SalesSummary';
 import RentalCalculator from '@/components/RentalCalculator';
@@ -19,7 +18,6 @@ const Index = () => {
       <main>
         <Hero />
         <RentalSummary />
-        <Catalog />
         <DJSection />
         <SalesSummary />
         <RentalCalculator />
