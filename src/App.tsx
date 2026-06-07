1f36a2c3aac2d48a219c118d563cd42746f9fc92
[dyad] <martinnovysedlak48@gmail.com>
2026-06-07 15:59:06 +0200

    [dyad] Opravené smerovanie v App.tsx nahradením neexistujúcej Ponuka.tsx za Prenajom.tsx a pridané chýbajúce stránky - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Ponuka from "./pages/Ponuka";
+import Prenajom from "./pages/Prenajom";
 import Predaj from "./pages/Predaj";
 import ProductDetail from "./pages/ProductDetail";
+import EquipmentDetail from "./pages/EquipmentDetail";
 import Blog from "./pages/Blog";
 import BlogPostDetail from "./pages/BlogPostDetail";
+import Kontakt from "./pages/Kontakt";
 import Admin from "./pages/Admin";
 import { Toaster } from "@/components/ui/sonner";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Router>
+    <>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/ponuka" element={<Ponuka />} />
+        <Route path="/prenajom" element={<Prenajom />} />
+        <Route path="/equipment/:id" element={<EquipmentDetail />} />
         <Route path="/predaj" element={<Predaj />} />
         <Route path="/predaj/:id" element={<ProductDetail />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:id" element={<BlogPostDetail />} />
+        <Route path="/kontakt" element={<Kontakt />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
       <Toaster position="top-right" theme="dark" />
-    </Router>
+    </>
   );
 }
 
