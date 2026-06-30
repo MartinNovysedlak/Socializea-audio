8d39b4599b157355828373c985c5d9a97b086855
[dyad] <martinnovysedlak48@gmail.com>
2026-06-30 18:08:59 +0200

    [dyad] wrote 1 file(s)

--- src/pages/Prenajom.tsx
+++ src/pages/Prenajom.tsx
@@ -1,27 +1,27 @@
-{
-  "id": "klub-maximal",
-  "name": "BALÍK 7: Klub MAXIMAL (380€ bez svetiel, 520€ so svetlami)",
-  "priceNoLights": 380,
-  "priceWithLights": 520,
-  "image": "https://images.unsplash.com/photo-1489641493513-ba4ee84ccee9?w=800",
-  "description": "Zameranie: Veľké diskotéky, stužkové pre viacero tried, festivalové stany nad 100 ľudí v interiéri. Extrémny zvukový tlak a laserová show.",
-  "soundSpecs": [
-    "1x Digitálny mixpult Behringer X Air 18",
-    "1x Riadiaci procesor the t.rack 4x4 (ideálne rozdelenie pásiem a ochrana reproduktorov pred preťažením)",
-    "2x Reproduktory Behringer B112 B112D",
-    "1x Sada 2 mikrofónov the t.bD",
-    "1x Sada 2 mikrofónov the t.bone free solo Twin HT",
-    "4x Subwoofer The Box Pro DSP 18 Sub (štvorica masívnych basákov)",
-    "2x Teleskopická stojanová tyč"
-  ],
-  "lightSpecs": [
-    "1x BeamZ SUSHI-DS",
-    "4x Rotujúca 90w Beam hlava",
-    "6x RGBWA UV Led Par svetlá",
-    "4x RGBW Led Bar 36w",
-    "1x Holografický Laser",
-    "2x Červeno-zelený Laser (párty efekt)",
-    "2x Dymostroj ADJ VF 1300 (udržiavanie stabilnej hmly)",
-    "1x Osvetľovacia konštrukcia"
-  ]
-}
+// Update the klub-maximal entry in the presetPackages array
+  {
+    id: "klub-maximal",
+    name: "BALÍK 7: Klub MAXIMAL (380€ bez svetiel, 520€ so svetlami)",
+    priceNoLights: 380,
+    priceWithLights: 520,
+    image: "https://images.unsplash.com/photo-1489641493513-ba4ee84ccee9?w=800",
+    description: "Zameranie: Veľké diskotéky, stužkové pre viacero tried, festivalové stany nad 100 ľudí v interiéri. Extrémny zvukový tlak a laserová show.",
+    soundSpecs: [
+      "1x Digitálny mixpult Behringer X Air 18",
+      "1x Riadiaci procesor the t.rack 4x4 (ideálne rozdelenie pásiem a ochrana reproduktorov pred preťažením)",
+      "2x Reproduktory Behringer B112D",
+      "1x Sada 2 mikrofónov the t.bone free solo Twin HT",
+      "4x Subwoofer The Box Pro DSP 18 Sub (štvorica masívnych basákov)",
+      "2x Teleskopická stojanová tyč"
+    ],
+    lightSpecs: [
+      "1x BeamZ SUSHI-DS",
+      "4x Rotujúca 90w Beam hlava",
+      "6x RGBWA UV Led Par svetlá",
+      "4x RGBW Led Bar 36w",
+      "1x Holografický Laser",
+      "2x Červeno-zelený Laser (párty efekt)",
+      "2x Dymostroj ADJ VF 1300 (udržiavanie stabilnej hmly)",
+      "1x Osvetľovacia konštrukcia"
+    ]
+  }
