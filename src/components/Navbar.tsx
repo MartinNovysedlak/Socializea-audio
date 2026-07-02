4600ec965b514847fe2d607f34aefbc14160d0c0
[dyad] <martinnovysedlak48@gmail.com>
2026-07-02 19:34:31 +0200

    [dyad] Oprava posúvania obsahu pri otvorení mobilného menu - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,15 +9,18 @@ const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const location = useLocation();
 
-  // Zablokovanie scrollovania na pozadí, keď je menu otvorené
   useEffect(() => {
     if (mobileMenuOpen) {
+      const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
       document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
     } else {
       document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     }
     return () => {
       document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     };
   }, [mobileMenuOpen]);
 
