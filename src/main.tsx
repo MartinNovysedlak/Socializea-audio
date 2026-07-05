b0537d6e9d92f9d5776d5f45590b26576e16a8f6
[dyad] <martinnovysedlak48@gmail.com>
2026-07-05 18:48:12 +0200

    [dyad] wrote 6 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,1 @@
-"use client";
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Toaster } from "sonner";
-import { DialogProvider } from './contexts/DialogContext';
-import './globals.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DialogProvider>
-      <Toaster position="top-right" />
-      <App />
-    </DialogProvider>
-  </React.StrictMode>
-);
+import { App } from './App';
