92f5c221523338307396e9be91bf700946fe2a2b
[dyad] <martinnovysedlak48@gmail.com>
2026-07-11 17:53:11 +0200

    [dyad] wrote 3 file(s)

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,21 +1,3 @@
-"use client";
+// ... all the existing code above ...
 
-// ... keep existing imports and code ...
-
-// Nájdeme volanie generateEmailHtml – v riadku asi 91
-// Zmeníme 'produkt' na 'product-inquiry'
-
-// Predtým:
-// const htmlContent = generateEmailHtml('produkt', { ... });
-
-// Po oprave:
-const htmlContent = generateEmailHtml('product-inquiry', {
-  name: `${inquiryFirstName} ${inquiryLastName}`,
-  email: inquiryEmail,
-  phone: inquiryPhone || 'Neuvedený',
-  date: 'Dopyt na produkt',
-  message: inquiryMessage,
-  packageName: product?.name || 'Neznámy produkt',
-});
-
-// ... keep rest of the file ...
+export default ProductDetail;
